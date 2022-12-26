742c81385943d5281e0134b8c03a270e702d62a9
SyedRaza13519 <mshahr786@gmail.com>
2022-12-26 10:43:11 +0500

    helo

--- migrations/20221219122032-create-game.js
+++ migrations/20221219122032-create-game.js
@@ -2,60 +2,50 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable(
-      "Games",
-      {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-        },
-        status: {
-          type: Sequelize.STRING,
-        },
-        turn: {
-          type: Sequelize.STRING,
-        },
-        winner: {
-          type: Sequelize.STRING,
-        },
+    await queryInterface.createTable("Games", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      status: {
+        type: Sequelize.STRING,
+      },
+      turn: {
+        type: Sequelize.STRING,
+      },
+      winner: {
+        type: Sequelize.STRING,
+      },
 
-        player1Id: {
-          type: Sequelize.INTEGER,
-          onDelete: "CASCADE",
-          allowNull: false,
-          references: {
-            model: "Users",
-            key: "id",
-          },
-        },
-        player2Id: {
-          type: Sequelize.INTEGER,
-          onDelete: "CASCADE",
-          allowNull: false,
-          references: {
-            model: "Users",
-            key: "id",
-          },
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
+      player1Id: {
+        type: Sequelize.INTEGER,
+        onDelete: "CASCADE",
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
         },
       },
-      {
-        uniqueKeys: {
-          Items_unique: {
-            fields: ["player1Id", "player2Id"],
-          },
+      player2Id: {
+        type: Sequelize.INTEGER,
+        onDelete: "CASCADE",
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
         },
-      }
-    );
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("Games");
